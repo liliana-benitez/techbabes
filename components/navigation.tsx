1979324aae6475d4856de899356b12bf9239f4e6
Liliana Benitez <lilyybenitezz@gmail.com>
2025-04-22 00:32:58 +0200

    nav and hero done

---
+++ components/navigation.tsx
@@ -0,0 +1,117 @@
+"use client"
+
+import Link from "next/link"
+import { useEffect, useState } from "react"
+import { Menu, X } from "lucide-react"
+import { usePathname } from "next/navigation"
+import Image from "next/image"
+import logo from "../public/logo.png"
+
+// Hook to detect screen size
+function useMediaQuery(query: string) {
+  const [matches, setMatches] = useState(false)
+
+  useEffect(() => {
+    const media = window.matchMedia(query)
+    if (media.matches !== matches) {
+      setMatches(media.matches)
+    }
+    const listener = () => setMatches(media.matches)
+    media.addEventListener("change", listener)
+    return () => media.removeEventListener("change", listener)
+  }, [matches, query])
+
+  return matches
+}
+
+interface NavItem {
+  id: number
+  content: string
+  url: string
+  dropdownItems?: { id: number; content: string; url: string }[]
+}
+
+export default function Navigation() {
+  const [nav, setNav] = useState(false)
+  const pathname = usePathname()
+  const isDesktop = useMediaQuery("(min-width: 768px)")
+
+  function handleNav() {
+    setNav(!nav)
+  }
+
+  useEffect(() => {
+    setNav(false)
+  }, [pathname])
+
+  const items: NavItem[] = [
+    { id: 1, content: "Home", url: "/" },
+    { id: 2, content: "Shop", url: "/about" },
+    {
+      id: 3,
+      content: "About",
+      url: "/about"
+    },
+
+    { id: 4, content: "Blog", url: "/blog" },
+    {
+      id: 5,
+      content: "Contact",
+      url: "/contact"
+    }
+  ]
+
+  return (
+    <div>
+      <div className="flex justify-between items-center px-10 py-3">
+        <Link href={"/"}>
+          <Image src={logo} alt="itsYamiG logo" height={50} width={50} />
+        </Link>
+
+        {/* DESKTOP NAV */}
+        {isDesktop && (
+          <ul className="flex gap-6">
+            {items.map((item) => {
+              return (
+                <li key={item.id}>
+                  <Link
+                    className="text-gray-500 font-semibold outline-none border-none"
+                    href={item.url}
+                  >
+                    {item.content}
+                  </Link>
+                </li>
+              )
+            })}
+          </ul>
+        )}
+
+        {/* MOBILE BURGER ICON */}
+        {!isDesktop && (
+          <div onClick={handleNav} className="block">
+            {nav ? <X /> : <Menu />}
+          </div>
+        )}
+      </div>
+
+      {/* MOBILE NAV */}
+      {!isDesktop && nav && (
+        <ul className="flex flex-col justify-center items-center gap-2 pb-6">
+          {items.map((item) => {
+            return (
+              <li key={item.id}>
+                <Link
+                  className="text-gray-500 font-semibold"
+                  href={item.url}
+                  onClick={() => setNav(false)}
+                >
+                  {item.content}
+                </Link>
+              </li>
+            )
+          })}
+        </ul>
+      )}
+    </div>
+  )
+}
