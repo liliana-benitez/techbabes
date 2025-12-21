2061d9cc2dc3678634fd5124dd6ab9ef432718e1
Liliana Benitez <lilyybenitezz@gmail.com>
2025-12-21 22:44:41 +0100

    complete contact us page

--- app/(pages)/contact/page.tsx
+++ app/(pages)/contact/page.tsx
@@ -5,9 +5,12 @@ import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Textarea } from "@/components/ui/textarea"
 import { z } from "zod"
-import { AlertCircleIcon, Mail, MapPin, Phone } from "lucide-react"
+import { AlertCircleIcon, Mail } from "lucide-react"
 import { Label } from "@/components/ui/label"
 import { Alert, AlertTitle } from "@/components/ui/alert"
+import Image from "next/image"
+import etsy from "../../../public/etsy.svg"
+import instagram from "../../../public/instagram.svg"
 
 const formSchema = z.object({
   name: z.string().min(2, "Name must be at least 2 characters."),
@@ -52,26 +55,60 @@ export default function ContactPage() {
   }
 
   return (
-    <div className="px-20 py-12">
-      <div className="grid md:grid-cols-2 gap-12 max-w-5xl mx-auto">
+    <div className="flex flex-col gap-12 px-20 py-12">
+      <h1 className="font-display font-bold text-3xl md:text-4xl">
+        Contact Us
+      </h1>
+      <div className="grid md:grid-cols-2 gap-12 mx-auto w-full">
         {/* Left info section */}
         <div className="space-y-6">
-          <h1 className="font-display font-bold text-3xl md:text-4xl">
-            Contact Us
-          </h1>
           <div className="flex items-start gap-4">
             <div className="bg-primary/10 p-3 rounded-full text-primary">
               <Mail className="h-6 w-6" />
             </div>
             <div>
-              <h3 className="font-bold text-lg">Email Us</h3>
-              <p className="text-muted-foreground">hello@techbabes.dev</p>
+              <h3 className="font-bold text-lg">Email</h3>
+              <p className="text-muted-foreground">
+                <a href="mailto:thetechbabes.inc@gmail.com"></a>
+                thetechbabes.inc@gmail.com
+              </p>
+            </div>
+          </div>
+          <div className="flex items-start gap-4">
+            <div className="bg-primary/10 p-3 rounded-full text-primary">
+              <Image
+                src={instagram}
+                alt="Instagram logo"
+                height={25}
+                width={25}
+              />
+            </div>
+            <div>
+              <h3 className="font-bold text-lg">Instagram</h3>
+              <p className="text-muted-foreground">
+                <a href="https://www.instagram.com/thetechbabes">
+                  @thetechbabes
+                </a>
+              </p>
+            </div>
+          </div>
+          <div className="flex items-start gap-4">
+            <div className="bg-primary/10 p-3 rounded-full text-primary">
+              <Image src={etsy} alt="Etsy logo" height={20} width={20} />
+            </div>
+            <div>
+              <h3 className="font-bold text-lg">Etsy</h3>
+              <p className="text-muted-foreground">
+                <a href="https://etsy.com/shop/TheTechBabes">
+                  etsy.com/shop/TheTechBabes
+                </a>
+              </p>
             </div>
           </div>
         </div>
 
         {/* Right form section */}
-        <div className="bg-card border border-border rounded-xl p-8 shadow-sm">
+        <div className="bg-card border border-border rounded-xl p-8 shadow-sm max-w-xl">
           <form onSubmit={handleSubmit} className="space-y-4">
             {/* Name */}
             <Label>Name</Label>
