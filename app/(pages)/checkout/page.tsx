11253c93f9ea378407e593067952fe40552f61f9
Liliana Benitez <lilyybenitezz@gmail.com>
2026-01-11 23:38:51 +0100

    change env variable

--- app/(pages)/checkout/page.tsx
+++ app/(pages)/checkout/page.tsx
@@ -15,9 +15,7 @@ import {
   useElements
 } from "@stripe/react-stripe-js"
 
-const stripePromise = loadStripe(
-  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!
-)
+const stripePromise = loadStripe(process.env.STRIPE_PUBLISHABLE_KEY!)
 
 const checkoutSchema = z.object({
   email: z.string().email("Invalid email address"),
