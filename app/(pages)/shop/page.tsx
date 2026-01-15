75cd81baeb6a394812cb401a731959763312889b
Liliana Benitez <lilyybenitezz@gmail.com>
2026-01-15 01:01:04 +0100

    trigger redeploy

--- app/(pages)/shop/page.tsx
+++ app/(pages)/shop/page.tsx
@@ -26,8 +26,6 @@ export default function Page() {
     getProducts()
   }, [])
 
-  console.log(products)
-
   const filteredProducts = products.filter((product) => {
     const matchesCategory =
       category === "All" ||
